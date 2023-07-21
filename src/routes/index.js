b2d6159edb3e400e002ebf26a7e131695d140d0e
unknown <giapthan0604@gmail.com>
2023-07-21 21:32:35 +0700

    update new

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,9 @@ const adminRouter = require('./admin');
 
 function route(app) {
   app.use('/admin', adminRouter);
+  app.use('/', (req, res) => {
+    res.redirect('/admin');
+  });
 }
 
 module.exports = route;
